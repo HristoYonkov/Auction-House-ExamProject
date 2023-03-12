c10c4fbafc9c7f696b0196a61d64266e2cf04429
Hristo Yonkov <hristopturs@gmail.com>
2023-03-12 15:23:36 +0200

    starting header

--- Junior-Dev-App/junior-dev/src/App.js
+++ Junior-Dev-App/junior-dev/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import { Header } from './components/header/Header';
 
 function App() {
   return (
     <>
-      <header className="App-header">
-        
-      </header>
+      <Header />
     </>
   );
 }
---
+++ Junior-Dev-App/junior-dev/src/components/header/Header.js
@@ -0,0 +1,23 @@
+// import { Link }
+
+export const Header = () => {
+
+    return (
+        <header>
+            <ul className="header-list">
+                <li className="header-list-item">
+                    asdasd
+                </li>
+                <li className="header-list-item">
+
+                </li>
+                <li className="header-list-item">
+
+                </li>
+                <li className="header-list-item">
+
+                </li>
+            </ul>
+        </header>
+    );
+}
