9d083f96458a11dbaea31b0f396077b916ea8596
Hristo Yonkov <hristopturs@gmail.com>
2023-03-22 15:20:46 +0200

    add html for Login and Register pages

--- Junior-Dev-App/junior-dev/src/components/Login/Login.js
+++ Junior-Dev-App/junior-dev/src/components/Login/Login.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './Login.css'
 
 export const Login = () => {
@@ -6,7 +8,21 @@ export const Login = () => {
     return(
         <>
         <section className="login">
-            <h1>Login</h1>
+
+            <form className='login-form'>
+                <h1>Login</h1>
+
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id='email' placeholder='email' />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id='password' placeholder='password' />
+
+                <p>If you don't have an account go to <Link to="/register">REGISTER</Link> page!</p>
+
+                <button>Login</button>
+            </form>
+
         </section>
         </>
     );
--- Junior-Dev-App/junior-dev/src/components/Register/Register.js
+++ Junior-Dev-App/junior-dev/src/components/Register/Register.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './Register.css'
 
 export const Register = () => {
@@ -6,7 +8,27 @@ export const Register = () => {
     return(
         <>
         <section className="register">
-            <h1>Register</h1>
+
+            <form className='register-form'>
+                <h1>Register</h1>
+
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" id='username' placeholder='username' />
+
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id='email' placeholder='email' />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id='password' placeholder='password' />
+
+                <label htmlFor="repass">Confirm Password</label>
+                <input type="password" name="repass" id='repass' placeholder='confirm password' />
+
+                <p>If you have an account go to <Link to="/login">LOGIN</Link> page!</p>
+
+                <button>Register</button>
+            </form>
+
         </section>
         </>
     );
