e1e6cadf84724decbfef03e25e986aad43d3881f
Hristo Yonkov <hristopturs@gmail.com>
2023-03-25 16:56:52 +0200

    updated css

--- Auction-House/auction-house/src/components/Details/Details.js
+++ Auction-House/auction-house/src/components/Details/Details.js
@@ -20,15 +20,18 @@ export const Details = () => {
                         <img src="https://g1-bg.cars.bg/2023-03-24_1/641d60a34ea732938e0fc562o.jpg" alt="car" />
 
                         <div className='details-content-middle-inner'>
-                            <p>Category: <span>Vehicles</span></p>
-                            <p>Description: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aliquam!</span></p>
-                            <p>Current price: <span>$3000</span></p>
+                            <p className='description-p'>Category: <span className='span-bold'>Vehicles</span></p>
+                            <p>Description: <br /> <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, aliquam!</span></p>
+                            <p>Current price: <span className='span-bold'>$3000</span></p>
+                            <button>Bid</button>
                         </div>
                     </div>
 
-                    <div className='details-button-wrapper'>
-                        <button><Link to="/details">Edit</Link></button>
-                        <button><Link to="/details">Delete</Link></button>
+                    <div className='details-footer'>
+                        <div className='details-button-wrapper'>
+                            <button><Link to="/details">Delete</Link></button>
+                            <button><Link to="/details">Edit</Link></button>
+                        </div>
                     </div>
                 </article>
 
