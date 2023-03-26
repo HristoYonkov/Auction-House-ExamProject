5b6a45a31d4c1986cdbd56053193946e2a0e24d1
Hristo Yonkov <hristopturs@gmail.com>
2023-03-26 15:30:34 +0300

    make register controlled form

--- Auction-House/auction-house/src/components/Register/Register.js
+++ Auction-House/auction-house/src/components/Register/Register.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Register.css'
 
 export const Register = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        repass: ''
+    });
 
+    const onChangeHandler = (e) => {
+        setFormData(state => ({ ...state, [e.target.name]: e.target.value }))
+    }
 
     return (
         <>
@@ -14,23 +24,50 @@ export const Register = () => {
 
                     <div className='input-wrapper'>
                         <label htmlFor="username">Username</label>
-                        <input type="text" name="username" id='username' placeholder='username' />
+                        <input
+                            type="text" name="username"
+                            id='username'
+                            placeholder='username'
+                            value={formData.username}
+                            onChange={onChangeHandler}
+                        />
                         <p className='err-msg'>Username must be between 2 and 10 characters long!</p>
                     </div>
 
                     <div className='input-wrapper'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id='email' placeholder='email' />
+                        <input
+                            type="email"
+                            name="email"
+                            id='email'
+                            placeholder='email'
+                            value={formData.email}
+                            onChange={onChangeHandler}
+                        />
                         <p className='err-msg'>Invalid E-mail!</p>
                     </div>
                     <div className='input-wrapper'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id='password' placeholder='password' />
+                        <input
+                            type="password"
+                            name="password"
+                            id='password'
+                            placeholder='password'
+                            value={formData.password}
+                            onChange={onChangeHandler}
+                        />
                         <p className='err-msg'>Password must be between 3 and 15 character's long!</p>
                     </div>
                     <div className='input-wrapper'>
                         <label htmlFor="repass">Confirm Password</label>
-                        <input type="password" name="repass" id='repass' placeholder='confirm-password' />
+                        <input
+                            type="password"
+                            name="repass"
+                            id='repass'
+                            placeholder='confirm-password'
+                            value={formData.repass}
+                            onChange={onChangeHandler}
+                        />
                         <p className='err-msg'>Password must be between 3 and 15 character's long!</p>
                         <p className='err-msg'>Password's must match!</p>
                     </div>
