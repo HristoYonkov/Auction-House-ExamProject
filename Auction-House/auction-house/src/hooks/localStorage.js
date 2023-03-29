f68f10809eeae82f9e6cae33e421d023fa5a6e78
Hristo Yonkov <hristopturs@gmail.com>
2023-03-29 10:39:34 +0300

    localstorage and auth service logic added

---
+++ Auction-House/auction-house/src/hooks/localStorage.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key, defaultValue) => {
+    const [value, setValue] = useState(() => {
+        const storeData = localStorage.getItem(key)
+
+        return (storeData && storeData !== undefined) ? JSON.parse(storeData) : defaultValue;
+    });
+
+    const setLocalStorageValue = (newValue) => {
+        if (newValue === undefined) {
+            newValue = {}
+        }
+        newValue.email
+            ? newValue = {
+                username: newValue.username,
+                email: newValue.email,
+                _id: newValue._id,
+                accessToken: newValue.accessToken
+            }
+            : newValue = {}
+        localStorage.setItem(key, JSON.stringify(newValue))
+        setValue(newValue);
+    }
+
+    return [value, setLocalStorageValue];
+}
---
+++ Auction-House/auction-house/src/services/authService.js
@@ -0,0 +1,48 @@
+const baseUrl = 'http://localhost:3030/auth';
+
+export const authLogin = async (data) => {
+    try {
+        const response = await fetch(`${baseUrl}/login`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            return result;
+        } else {
+            throw new Error(result.error);
+        }
+
+    } catch (error) {
+        return error;
+    }
+}
+
+export const authRegister = async (data) => {
+    try {
+        const response = await fetch(`${baseUrl}/register`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            return result
+        } else {
+            throw new Error(result.error);
+        }
+
+    } catch (error) {
+        return error
+    }
+
+}
