eb49d024e5e31f3b776d10e9e13f0eb3d079b2ba
Hristo Yonkov <hristopturs@gmail.com>
2023-04-05 23:27:30 +0300

    search functionality polished!

--- Auction-House/auction-house/src/components/Search/Search.js
+++ Auction-House/auction-house/src/components/Search/Search.js
@@ -1,19 +1,99 @@
-import './Search.css';
+import { useState } from 'react';
 
+import './Search.css';
+import * as listingSerive from '../../services/listingService';
+import { SingleSearch } from './SingleSearch/SingleSearch';
 export const Search = () => {
+    const [searchResult, setSearchResult] = useState([]);
+    const [value, setValue] = useState('');
+    const [category, setCategory] = useState('');
+
+    const onChangeTypeHandler = (e) => {
+        setCategory(e.target.value)
+    }
+
+    const onChangeValueHandler = (e) => {
+        setValue(e.target.value)
+    }
+
+    const resetHandler = () => {
+        setCategory('')
+        setValue('')
+    }
+
+    const searchHandler = async () => {
+        const response = await listingSerive.getAll();
+        let result = [];
+        
+        if (response.length > 0 && category === '' && value !== '') {
+            response.filter(l => {
+                if (l.title.startsWith(value)) {
+                    result.push(l);
+                }
+            })
+        }
+
+        if (response.length > 0 && category !== '') {
+            response.filter(l => {
+                if (l.category === category) {
+                    result.push(l)
+                }
+            })
+            
+            if (result.length > 0 && value !== '') {
+                // console.log(result);
+                // result.filter(l => !l.title.includes(value))
+                let newResult = [];
+                result.forEach(l => {
+                    if (l.title.includes(value)) {
+                        newResult.push(l);
+                    }
+                })
+                result = newResult;
+            }
+        }
+        if (category === '' && value === '') {
+            setSearchResult(response);
+            
+        } else {
+            setSearchResult(result);
+        }
+    }
 
     return (
-        <>
+        <div className='search-section'>
             <div>
                 <h1 className='search-header'>Search for listing..</h1>
             </div>
-            <div className='search-wrapper'>
-                <input type="text" id='search' placeholder='Type your search here..' name='search' className='search' />
-                <button className='search-btn'>Search</button>
-            </div>
-            <section className='searchResults'>
 
+            <div className='search-wrapper'>
+                <div className='search-wrapper-inputs'>
+                    <div className='input-btn-wrapper'>
+                        <input onChange={onChangeValueHandler} value={value} name='search' type="text" id='search' className='search' placeholder='Type your search here..' />
+                        <button onClick={searchHandler} className='search-btn'>Search</button>
+                        <button onClick={resetHandler} className='search-btn'>Reset</button>
+                    </div>
+                    <select
+                        className='select-input'
+                        name="category"
+                        id="category"
+                        value={category}
+                        onChange={onChangeTypeHandler}
+                    >
+                        <option value="">Choose option..</option>
+                        <option value="Vehicles">Vehicles</option>
+                        <option value="Computers">Computers</option>
+                        <option value="Home Appliances">Home Appliances</option>
+                        <option value="Others">Others</option>
+                    </select>
+                </div>
+            </div>
+
+            <section className='search-result'>
+                {searchResult.length > 0 &&
+                    searchResult.map(x => <SingleSearch key={x._id} listing={x} />)
+                }
             </section>
-        </>
+        </div>
     )
 }
---
+++ Auction-House/auction-house/src/components/Search/SingleSearch/SingleSearch.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+import './SingleSearch.css';
+
+export const SingleSearch = ({ listing }) => {
+
+    return (
+        <article className='card'>
+            <h3>{listing.title}</h3>
+            <div className="card-image-holder">
+                <img src={listing.imageUrl} alt="someImage" />
+            </div>
+            <p>Category: <span>{listing.category}</span></p>
+            <p>Current price: <span>${listing.price}</span></p>
+            <Link type='button' to={`/details/${listing._id}`}><button>Details</button></Link>
+        </article>
+    )
+}
