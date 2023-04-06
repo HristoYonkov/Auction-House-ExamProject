a04b62f7cedbbb42ef5c6f64a28b8b095bd0a154
Hristo Yonkov <hristopturs@gmail.com>
2023-04-06 18:29:20 +0300

    add cloud database

--- Auction-House/server/connectionString.js
+++ Auction-House/server/connectionString.js
@@ -1,4 +1,5 @@
-const connectionString = "mongodb://127.0.0.1:27017/auction-house";
+// const connectionString = "mongodb://127.0.0.1:27017/auction-house";
+const connectionString = "mongodb+srv://Swiftpaw:1234567890@cluster0.qhvpfqf.mongodb.net/Auction-House";
 module.exports = {
     connectionString,
 };
