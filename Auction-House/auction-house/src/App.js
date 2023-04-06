b686b7d4f04dc5f5ee5efafd69ae4fa75dc4ec6e
Hristo Yonkov <hristopturs@gmail.com>
2023-04-06 15:07:59 +0300

    Update App.js

--- Auction-House/auction-house/src/App.js
+++ Auction-House/auction-house/src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import './App.css';
 import { useLocalStorage } from './hooks/localStorage';
 
-import { Header } from './components/Header/Header';
+import { Header } from './components/Header/Header.js';
 import { Footer } from './components/Footer/Footer';
 import { Home } from './components/Home/Home';
 import { Catalog } from './components/Catalog/Catalog';
