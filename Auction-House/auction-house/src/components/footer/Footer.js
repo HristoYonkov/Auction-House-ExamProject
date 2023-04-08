beabf81240bc08ed76a93cdfd09c173e6b00e178
Hristo Yonkov <hristopturs@gmail.com>
2023-04-08 15:11:43 +0300

    update footer

--- Auction-House/auction-house/src/components/footer/Footer.js
+++ Auction-House/auction-house/src/components/footer/Footer.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import './Footer.css';
+import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
 export const Footer = () => {
@@ -87,7 +88,17 @@ export const Footer = () => {
                 </article>
             </section> */}
 
-            <footer>Copyright © 2023 AuctionHouse. <span>Hristo Yonkov@</span><span id='sf-logo'>SoftUni</span> - All Rights Reserved.</footer>
+            <footer>
+                <div className="footer-icons">
+                    <Link to="https://www.facebook.com/profile.php?id=100010592597191"><i class="fa-brands fa-facebook-f"></i></ Link>
+                    <Link to="https://github.com/2HEADSs"><i className="fa-brands fa-square-github"></i></Link>
+                    <Link to="https://www.linkedin.com/in/pavel-dimitrov-b6a503229/"><i className="fa-brands fa-linkedin"></i></Link>
+                </div>
+                <div>
+                    Copyright © 2023 AuctionHouse. <span>Hristo Yonkov@</span><span id='sf-logo'>SoftUni</span> - All Rights Reserved.
+                </div>
+                <div className='footer-divider'></div>
+            </footer>
         </div>
     );
 }
