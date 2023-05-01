342b8424ae2cef67cb19fcbf593908a040a8879a
Hristo Yonkov <hristopturs@gmail.com>
2023-05-01 21:27:41 +0300

    fix the animation bug in catalog

--- Auction-House/auction-house/src/components/Catalog/OneListing.js
+++ Auction-House/auction-house/src/components/Catalog/OneListing.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 export const OneListing = ({ listing }) => {
     return (
-        <article className='card'>
+        <article className='catalog-card'>
             <h3>{listing.title}</h3>
             <div className="card-image-holder">
                 <img src={listing.imageUrl} alt="someImage" />
